19dbb090a7e28dd239b74c2ba42652dc75ef4c5b
venkykanuri <venkateshv020@gmail.com>
2021-09-28 17:14:19 +0530

    Initial commit

---
+++ htmldom1.js
@@ -0,0 +1,7 @@
+function keydown() {
+    alert("dont press a key in input");
+}
+fuction keyup(){
+    document.getElementById("new").style.backgroundColor ="yellow"
+    document.getElementById("new").
+}
